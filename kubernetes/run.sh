1960bcc756d67a6772fd4b2af5110ccce6538c3a
kiwenlau <kiwenlau@126.com>
2016-01-05 11:33:10 +0900

    eliminate start script

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo -e "\nsudo docker build -t kiwenlau/kubernetes-cluster:1.0.7 ."
-sudo docker build -t kiwenlau/kubernetes-cluster:1.0.7 .
+echo -e "\nsudo docker build -t kiwenlau/kubernetes-master:1.0.7 ."
+sudo docker build -t kiwenlau/kubernetes-master:1.0.7 .
 
 docker rm -f kubernetes-master > /dev/null
 
-rm tmp/*
-
-echo -e "\n\ndocker run -it -d kiwenlau/kubernetes-cluster:1.0.7 /usr/local/bin/start-kubernetes.sh master ."
-docker run -it -d --name=kubernetes-master -v /root/mac/kubernetes-cluster-docker/kubernetes/tmp:/tmp kiwenlau/kubernetes-cluster:1.0.7 
+echo -e "\n\ndocker run -it -d kiwenlau/kubernetes-master:1.0.7"
+docker run -it -d --name=kubernetes-master kiwenlau/kubernetes-master:1.0.7 
