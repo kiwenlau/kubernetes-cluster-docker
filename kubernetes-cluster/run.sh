71a38fe2dd2fe30f2e64f140feb5e1551d16cabd
kiwenlau <kiwenlau@126.com>
2016-01-05 12:28:27 +0900

    test

--- kubernetes-cluster/run.sh
+++ kubernetes-cluster/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -e "\nsudo docker build -t kiwenlau/kubernetes-cluster:1.0.7 ."
+echo -e "\nBuild kiwenlau/kubernetes-cluster:1.0.7 image"
 sudo docker build -t kiwenlau/kubernetes-cluster:1.0.7 .
 
 docker rm -f kubernetes-master kubernetes-slave > /dev/null
 
-echo -e "\n\ndocker run -it -d kiwenlau/kubernetes-cluster:1.0.7"
+echo -e "\n\nRun kubernetes-master container"
 docker run -it -d --name=kubernetes-master kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-master.conf 
 
-echo -e "\n\ndocker run -it -d kiwenlau/kubernetes-cluster:1.0.7"
+echo -e "\n\nRun kubernetes-slave container"
 docker run -it -d --link kubernetes-master:kubernetes-master --privileged  -v /var/run/docker.sock:/var/run/docker.sock --name=kubernetes-slave kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-slave.conf 
--- rebuild-images.sh
+++
@@ -1,5 +0,0 @@
-cd kubernetes/
-
-sudo docker build -t kiwenlau/kubernetes:1.0 .
-
-cd ..
