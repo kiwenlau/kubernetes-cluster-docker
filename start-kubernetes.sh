3a93f43eba2e22ac465e2489044f16bf37900993
kiwenlau <kiwenlau@126.com>
2016-01-05 15:29:23 +0900

    run 3 slaves

--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -3,13 +3,19 @@
 echo -e "\nBuild kiwenlau/kubernetes-cluster:1.0.7 image"
 sudo docker build -f ./kubernetes-cluster/Dockerfile -t kiwenlau/kubernetes-cluster:1.0.7 ./kubernetes-cluster
 
-docker rm -f kubernetes-master kubernetes-slave > /dev/null
+docker rm -f kubernetes-master kubernetes-slave1 kubernetes-slave2 kubernetes-slave3 > /dev/null
 
 echo -e "\nRun kubernetes-master container"
 docker run -it -d --name=kubernetes-master kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-master.conf 
 
-echo -e "\n\nRun kubernetes-slave container"
-docker run -it -d --link kubernetes-master:kubernetes-master --privileged  --name=kubernetes-slave kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-slave.conf 
+echo -e "\n\nRun kubernetes-slave1 container"
+docker run -it -d --link kubernetes-master:kubernetes-master --privileged  --name=kubernetes-slave1 kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-slave.conf 
+
+echo -e "\n\nRun kubernetes-slave2 container"
+docker run -it -d --link kubernetes-master:kubernetes-master --privileged  --name=kubernetes-slave2 kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-slave.conf
+
+echo -e "\n\nRun kubernetes-slave3 container"
+docker run -it -d --link kubernetes-master:kubernetes-master --privileged  --name=kubernetes-slave3 kiwenlau/kubernetes-cluster:1.0.7 /usr/bin/supervisord --configuration=/etc/supervisor/conf.d/kubernetes-slave.conf  
 
 echo -e "\n\nGet into kubernetes-master container"
 docker exec -it kubernetes-master bash
